f59a375b6586562b58de3ec9bd76305dbce06ac2
Tanmay5454 <tiwaritanmay424@gmail.com>
2025-01-29 11:34:07 +0530

    Playlist

---
+++ Sorting and Searching/Playlist.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define yes cout<<"Yes"<<endl
+#define no cout<<"No"<<endl
+#define alice cout<<"Alice"<<endl
+#define bob cout<<"Bob"<<endl
+#define fo(a,b) for(i=a;i<b;i++)
+#define foj(a,b) for(j=a;j<b;j++)
+#define int int long long
+using namespace std;
+
+
+
+
+//Frequently used Functions
+string DecToBi(int n) {
+ string bin = ""; while (n>0) { bin += to_string(n%2); n=n/2; } for (int i = 0; i < bin.size() / 2; i++) { swap(bin[i], bin[bin.size() - i - 1]); } return bin;
+ }
+int BiToDec(string str) {
+ int dec_num = 0; int power = 0 ; int n = str.length() ; for(int i = n-1 ; i>=0 ; i--){ if(str[i] == '1'){ dec_num += (1<<power) ; } power++ ; } return dec_num; 
+ }
+bool isPrime(int n) {
+ if (n <= 1) return false; for (int i = 2; i <= n / 2; i++) if (n % i == 0) return false; return true; 
+ }
+bool sortbysec(const pair<int,int> &a,const pair<int,int> &b){
+return (a.second < b.second);
+}
+//***************************************************************************************************************************************************
+
+
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int i,j,n;
+    cin>>n;
+    int a[n];
+    fo(0,n)
+    cin>>a[i];
+    map<int,int> m;
+    j=0;
+    int c=0,ans=0;
+    fo(0,n){
+        if(m[a[i]]==0){
+            c++;
+            m[a[i]]++;
+        }
+        else{
+            while(m[a[i]]!=0 && j<i){
+                m[a[j]]--;
+                c--;
+                j++;
+            }
+            i--;
+        }
+        // cout<<c<<" "; 
+        ans=max(ans,c);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
